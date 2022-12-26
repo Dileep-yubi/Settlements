56c807ed2ed8e0ddd3eea7fdae897f9e200442b5
samuelprasad-yubi <samuel.prasad@go-yubi.com>
2022-12-26 10:27:00 +0530

    refactor: removes unused imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
 import { todoState } from "./todo/reducer";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
-import reduxSaga from "redux-saga";
-import { rootSaga } from "./sagas";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
-import { takeEvery } from "redux-saga/effects";
-import { TodoActionTypes } from "./todo/types";
 import { watchTodo } from "./todo/sagas";
 
 export const initialState = {
